d5011ce9bd0c04832b54846459520a66ae06cfbb
NuclearMystic <solarcompost@gmail.com>
2025-01-04 12:10:14 -0500

    initial commit

---
+++ source/PrepItem.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+class PrepItem {
+    constructor(name, unitPrefix = "", isFrozen = false) {
+        this.name = name;
+        this.unitPrefix = unitPrefix;
+        this.isFrozen = isFrozen;
+    }
+}
+
+
